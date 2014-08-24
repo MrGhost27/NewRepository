9d93b4f38e20848f732d073fb24b7279e72b5f1a
Chris Mullen <cbmullen@gmail.com>
2014-08-24 20:38:50 +0100

    Signed-off-by: Chris Mullen <cbmullen@gmail.com>

---
+++ CharacterCreationandDevelopment/CharacterCreationandDevelopment/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharacterCreationandDevelopment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Character_Creation CharCreation = new Character_Creation();
+            CharCreation.Show();
+            this.Hide();
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        }
+    }
+}
---
+++ CharacterCreationandDevelopment/CharacterCreationandDevelopment/HelperClass/HelperClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCreationandDevelopment
+{
+    static class HelperClass
+    {
+        private static Random random = new Random();
+        private static List<Image> imageList = new List<Image>();
+
+        public static List<Image> Images()
+        {
+            string[] images = System.IO.Directory.GetFiles(@"Images");
+            foreach (string Img in images)
+            {
+                Bitmap bmp = new Bitmap(Img);
+                imageList.Add(bmp);
+            }
+            return imageList;
+        }
+
+        public static string RandomName()
+        {
+            /*Ensure you map this correctly in filepath. Also Ensure File properties has "embedded resource"
+             * as the build action. Copy to Output directory if newer
+             */
+
+            string filepath = @"HelperClass\ListOfNames.txt";
+            string[] names = System.IO.File.ReadAllLines(filepath);
+
+            return names[GenerateRandomNumber(1, names.Count())];
+
+        }
+
+        public static int GenerateRandomNumber(int min, int max)
+        {
+            int randomNumber = random.Next(min, max);
+            return randomNumber;
+        }
+
+        public static string RandomCityName()
+        {
+            string filepath = @"HelperClass\ListOfCities.txt";
+            string[] cityNames = System.IO.File.ReadAllLines(filepath);
+
+            return cityNames[GenerateRandomNumber(1, cityNames.Count())];
+        }
+    }
+}
