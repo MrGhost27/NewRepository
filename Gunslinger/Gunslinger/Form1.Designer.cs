abb01927dd5d187584c2ff820a9f1d17d8ecc9ac
MrGhost27 <Ghost27@live.co.uk>
2014-08-25 20:25:01 +0100

    Why can't the methods see the "Global References" of statsHolder and randomSeed?

--- Gunslinger/Gunslinger/Form1.Designer.cs
+++ Gunslinger/Gunslinger/Form1.Designer.cs
@@ -123,7 +123,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Gunslinger Game";
             this.ResumeLayout(false);
             this.PerformLayout();
 
